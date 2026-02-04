0a1a6c45eede196a9e92e52da674ae92a50335e8
mariblan <mariablancocanal@gmail.com>
2026-02-04 15:54:04 +0100

    Changed base

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Thinking_In_React/",
+  base: "/ThinkingInReact/",
   plugins: [react(), tailwindcss()],
 })
